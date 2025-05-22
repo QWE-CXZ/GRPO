710d145f0b3a636f401e3ed2341ebcc169653858
QWE-CXZ <1456355244@qq.com>
2025-05-22 23:57:28 +0800

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -6,7 +6,7 @@ USER_INSTRUCTION="""
 分析这段文本的安全性，并按照[安全,偏见歧视,道德法律,身心伤害,财产与隐私,攻击冒犯]为文本打上标签：
 """
 SYSTEM_PROMPT="""
-并使用中文按以下格式回答问题:
+使用中文按以下格式回答问题:
 <reasoning>
 ...
 </reasoning>
@@ -14,17 +14,12 @@ SYSTEM_PROMPT="""
 ...
 </answer>
 """
-file_path="gen_data.json"
+file_path="data/raw_data.json"
 with open(file_path, "r", encoding="utf-8") as f:
     datas = json.load(f)
 processed_data=[]
 for data in datas:
     instruction=USER_INSTRUCTION+data['context']
-    prompt = [
-        {"role": "system", "content": SYSTEM_PROMPT},
-        {"role": "user", "content": instruction}
-    ]
-
     label_num=len(data['answer'])
     if label_num>1 and label_num==2:
         answer='['+data['answer'][0]+','+data['answer'][1]+']'
@@ -33,10 +28,18 @@ for data in datas:
     elif label_num>2:
         continue
     output=f"<reasoning>\n{data['reasoning']}\n</reasoning>\n<answer>\n{answer}\n</answer>"
-    processed_data.append({"prompt": prompt, "response": output})
+    processed_data.append({"prompt": SYSTEM_PROMPT,"instruction":instruction, "response": output})
 dataset = Dataset.from_list(processed_data)
-# for i in range(10):
-#     print("------------------")
-#     print(dataset[i]['response'])
-save_path='raw_data'
-dataset.save_to_disk(save_path)
+sft_dataset=dataset.select(range(500))
+print(sft_dataset.shape)
+grpo_dataset=dataset.select(range(500,2000))
+print(grpo_dataset.shape)
+testset=dataset.select(range(2000,dataset.shape[0]))
+print(testset.shape)
+print(testset[0]['response'])
+sft_save_path="data/sft_data"
+grpo_save_path="data/grpo_data"
+testset_save_path="data/test_data"
+sft_dataset.save_to_disk(sft_save_path)
+grpo_dataset.save_to_disk(grpo_save_path)
+testset.save_to_disk(testset_save_path)
